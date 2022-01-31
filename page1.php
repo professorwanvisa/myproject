516e3177828454706ad1633eedbc8a9d13245850
professorwanvisa <97584441+professorwanvisa@users.noreply.github.com>
2022-01-31 20:04:38 +0700

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title style="colr:blue;">ประวัติส่วนตัว</title>
+    <br>
+    <br>
+  <head>
+  <body>
+    <h1><b>ชื่อ : </b><i>วรรณวิสา นิตยพันธ์</i></h1>
+  </body>
+<html>
