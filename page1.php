24c71dd918f6b1b49be8d8411d955725a1b6009f
professorwanvisa <97584441+professorwanvisa@users.noreply.github.com>
2022-02-01 18:56:17 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,10 +1,20 @@
 <html>
-  <head>
-    <title style="colr:blue;">ประวัติส่วนตัว</title>
+  <head> 
+    <title>page 1</title> 
+  </head>
+  <body bgcolor="lightblue">
+    <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
+    <br> 
+    <b>ชื่อ :</b> <i>วรรณวิสา นิตยพันธ์</i>
+    <br> 
+    <b>ชื่อเล่น :</b> <i>ไข่หวาน</i> 
     <br>
+    <b>ชั้น :</b> <i>ม.4/12</i>
+    <br> 
+    <b>เลขที่ :</b> <i>26</i>
     <br>
-  <head>
-  <body>
-    <h1><b>ชื่อ : </b><i>วรรณวิสา นิตยพันธ์</i></h1>
-  </body>
-    <html>
+    <b>เลขประจำตัวนักเรียน :</b> <i>57070</i>
+    <br>
+    <b>โรงเรียน :</b> <i>หล่มสักวิทยาคม<i> 
+    </body>
+    </html>
