a7553e136b2d96e63cefe8570c1353114c64ae0b
professorwanvisa <97584441+professorwanvisa@users.noreply.github.com>
2022-02-09 20:01:56 +0700

    Create mypage1.php

---
+++ mypage1.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>การเชื่อมโยงเว็บไซต์</title>
+  </head>
+  <body>
+    <a href="https://facebook.com"><h1>จิ้มจิ้ม</h1></a>
+    <a href="contact.php"><h1>ติดต่อฉัน</h1></a>
+  </body>
+</html>
