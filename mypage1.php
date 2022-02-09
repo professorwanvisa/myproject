8476d00737febd6359e995a2cc90c93b1e4dc455
professorwanvisa <97584441+professorwanvisa@users.noreply.github.com>
2022-02-09 20:03:28 +0700

    Update mypage1.php

--- mypage1.php
+++ mypage1.php
@@ -4,6 +4,6 @@
   </head>
   <body>
     <a href="https://facebook.com"><h1>จิ้มจิ้ม</h1></a>
-    <a href="contact.php"><h1>ติดต่อฉัน</h1></a>
+    <a href="mypage2.php"><h1>page 2</h1></a>
   </body>
 </html>
