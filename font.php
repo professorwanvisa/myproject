95b3d2bef06c1faa7cc4207e3a7ed6c359fc8d4e
professorwanvisa <97584441+professorwanvisa@users.noreply.github.com>
2022-02-23 13:34:24 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("TA16BIT-Regular.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("sOv_kLoRnG.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">วรรณวิสา นิตยพันธ์ ม.4/12 เลขที่ 24 เลขประจำตัว 57070</h1>
+    <h1 id="font2">วรรณวิสา นิตยพันธ์ ม.4/12 เลขที่ 24 เลขประจำตัว 57070</h1>
+  </body>
+</html>
