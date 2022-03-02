090dbeaebacb30a3f901f27ab0da200c0776279d
professorwanvisa <97584441+professorwanvisa@users.noreply.github.com>
2022-03-02 13:07:50 +0700

    Update table.php

--- table.php
+++ table.php
@@ -3,13 +3,12 @@
     <title>การสร้างตาราง</title>
   </head>
   <body>
-    <table>
+    <table border="1">
       <tr>
         <td>column 1</td>
         <td>column 2</td>
         <td>column 3</td>
       </tr>
     </table>
-  
   </body>
 </html>
