e802ff47a2e7028ee4cc25010e9dadf17df7ffa9
professorwanvisa <97584441+professorwanvisa@users.noreply.github.com>
2022-03-02 14:36:20 +0700

    Update table.php

--- table.php
+++ table.php
@@ -12,9 +12,9 @@
       </tr>
       <tr>
         <td>1</td>
-        <td>Tony Stark</td>
-        <td><img src="https://ae01.alicdn.com/kf/Hf763d459ba5f4c1c956acb0b5c3c9d3cA/IVSTA-Tony-Stark-Anti-Blue-Light-Gaming.jpg" width="45px"></td>
-        <td><a href="https://twitter.com/realtonystark">Twitter</a></td>
+        <td>Noah Schnapp</td>
+        <td><img src="https://akns-images.eonline.com/eol_images/Entire_Site/2020726/rs_1200x1200-200826041703-1200-Noah-Schnapp-LT-082620-shutterstock_editorial_10323173bu.jpg?fit=around%7C1080:1080&output-quality=90&crop=1080:1080;center,top" width="45px"></td>
+        <td><a href="https://twitter.com/noah_schnapp">Twitter</a></td>
       </tr> 
     </table>
   </body>
