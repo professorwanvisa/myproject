bd45a77f131cb428994c6b8d26fce5235d0a6c10
professorwanvisa <97584441+professorwanvisa@users.noreply.github.com>
2022-03-02 13:13:03 +0700

    Update table.php

--- table.php
+++ table.php
@@ -5,9 +5,9 @@
   <body>
     <table border="1">
       <tr>
-        <td>column 1</td>
-        <td>column 2</td>
-        <td>column 3</td>
+        <th>column 1</th>
+        <th>column 2</th>
+        <th>column 3</th>
       </tr>
       <tr>
         <td>column 1</td>
