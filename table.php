28256b82c7cfde3cc7277d3c39b7457a692ab12b
professorwanvisa <97584441+professorwanvisa@users.noreply.github.com>
2022-03-02 13:06:16 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td>column 1</td>
+        <td>column 2</td>
+        <td>column 3</td>
+      </tr>
+    </table>
+  
+  </body>
+</html>
