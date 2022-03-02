3206d52ed5baa658bf8c1b7cb5e7afd5dcb30e82
professorwanvisa <97584441+professorwanvisa@users.noreply.github.com>
2022-03-02 14:51:28 +0700

    Update table.php

--- table.php
+++ table.php
@@ -16,6 +16,30 @@
         <td><img src="https://akns-images.eonline.com/eol_images/Entire_Site/2020726/rs_1200x1200-200826041703-1200-Noah-Schnapp-LT-082620-shutterstock_editorial_10323173bu.jpg?fit=around%7C1080:1080&output-quality=90&crop=1080:1080;center,top" width="45px"></td>
         <td><a href="https://twitter.com/noah_schnapp">Twitter</a></td>
       </tr> 
+      <tr>
+        <td>2</td>
+        <td>Millie Bobby Brown</td>
+        <td><img src="https://media.vanityfair.com/photos/5d6459d47a1e590008d38567/1:1/w_1332,h_1332,c_limit/millie-bobby-brown-florence-by-mills.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/milliestopshate">Twitter</a></td>
+      </tr> 
+      <tr>
+        <td>3</td>
+        <td>Finn Wolfhard</td>
+        <td><img src="https://media.glamourmagazine.co.uk/photos/6138c5c669a8a3323e6d9c5e/3:4/w_1278,h_1704,c_limit/Finn-Wolfhard-10nov17-rexfeatures_9176184eq-b.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/finnwolfhard">Twitter</a></td>
+      </tr> 
+      <tr>
+        <td>4</td>
+        <td>Natalia Dyer</td>
+        <td><img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1158961376.jpg?crop=0.668xw:1.00xh;0.167xw,0&resize=640:*" width="45px"></td>
+        <td><a href="https://twitter.com/nataliadyer">Twitter</a></td>
+      </tr> 
+      <tr>
+        <td>5</td>
+        <td>Charlie Heaton</td>
+        <td><img src="https://www.thesun.co.uk/wp-content/uploads/2017/10/NINTCHDBPICT000501579516.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/officialcheaton">Twitter</a></td>
+      </tr> 
     </table>
   </body>
 </html>
